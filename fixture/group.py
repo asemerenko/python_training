520a3af796673fabe2bc69897388b7a42bf1578b
asemerenko <anna.semerenko@djangostars.com>
2019-06-17 15:22:54 +0300

    Общая для всех фикстура

--- fixture/group.py
+++ fixture/group.py
@@ -24,4 +24,17 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
         self.open_groups_page()
+        # Select first group
+        wd.find_element_by_name("selected[]").click()
+        # Submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
