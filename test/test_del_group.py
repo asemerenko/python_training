78f9f9cd36cdd1a4e03161625a3c2a2547b576e3
asemerenko <anna.semerenko@djangostars.com>
2019-06-18 17:20:15 +0300

    Обеспечtyj выполнение предусловий в тестах для удаления и модификации для групп и контактов

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_delete_first_group(app):
+def test_delete_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="New_group_for_delete"))
     app.group.delete_first_group()
