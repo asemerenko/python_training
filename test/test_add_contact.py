3aee0191396d6dca2b433a977b5d490f93978fbe
asemerenko <anna.semerenko@djangostars.com>
2019-07-11 23:02:25 +0300

    Реализована загрузку тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from data.contacts import testdata
+# import pytest
+# from data.contacts import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact_hanna(app, contact):
+# @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact_hanna(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts)+1 == app.contact.count()
