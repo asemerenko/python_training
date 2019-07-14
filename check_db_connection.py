f5057f42c13a1ba191c62619e7bf5aa74f10cd51
asemerenko <anna.semerenko@djangostars.com>
2019-07-14 22:59:36 +0300

    Фикстура для взаимодействия с базой данных

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,12 @@
 import pymysql.cursors
 
 
-connecton = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 
 try:
-    cursor = connecton.cursor()
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM group_list")
     for row in cursor.fetchall():
         print(row)
 finally:
-    connecton.close()
+    connection.close()
