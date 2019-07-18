5b9fa7380e8fc6201f18683b113904dbbeaf201c
asemerenko <anna.semerenko@djangostars.com>
2019-07-18 21:34:34 +0300

    Тесты для проверки информации о контактах

--- test/test_db_contact_matches_ui.py
+++ test/test_db_contact_matches_ui.py
@@ -5,6 +5,6 @@ def test_contact_list(app, db):
     ui_list = app.contact.get_contact_list()
 
     def clean(contact):
-        return Contact(id=contact.id, firstname=contact.firstname.strip())
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
     db_list = list(map(clean, db.get_contact_list()))
     assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
