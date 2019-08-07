14b31dabad2537b0304a6c49b78fb12c3686e8bc
asemerenko <anna.semerenko@djangostars.com>
2019-08-07 16:55:29 +0300

    Robot Framework - 1

---
+++ rf/AddressBook.py
@@ -0,0 +1,50 @@
+from fixture.application import Application
+import json
+import os.path
+from fixture.db import DbFixture
+from model.group import Group
+
+
+class AddressBook:
+
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+
+    def __init__(self, config="target.json", browser="chrome"):
+        self.browser = browser
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", config)
+        with open(config_file) as f:
+            self.target = json.load(f)
+
+    def init_fixtures(self):
+        web_config = self.target['web']
+        self.fixture = Application(browser=self.browser, base_url=web_config["baseUrl"])
+        self.fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+        db_config = self.target['db']
+        self.dbfixture = DbFixture(host=db_config['host'], name=db_config['name'],
+                                   user=db_config['user'], password=db_config['password'])
+
+    def destroy_fixtures(self):
+        self.fixture.destroy()
+        self.dbfixture.destroy()
+
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
+
+    def delete_group(self, group):
+        self.fixture.group.delete_group_by_id(group.id)
+
+    def modify_group(self, group1, group2):
+        self.fixture.group.modify_group_by_id(group1.id, group2)
+
+    def group_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
+    def verify_group_list_is_not_empty(self, list1):
+        if len(list1) == 0:
+            self.fixture.group.create(Group(name="New_group"))
